cf91a28f8037f1066e477e954f1098131ea564e7
ash-silver <passlion@naver.com>
2023-01-13 19:09:16 +0900

    1/13,19:09

--- Project/src/main/java/com/project/controller/QnABoardController.java
+++ Project/src/main/java/com/project/controller/QnABoardController.java
@@ -3,6 +3,7 @@ package com.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.service.QnABoardService;
@@ -19,4 +20,8 @@ public class QnABoardController {
 		return "qnaboard";
 	}
 	
+	@PostMapping("") 
+	public String addQna() {
+		return "qnaboard";
+	}
 }
