f75d41a9b5cfedb759b4343169059e045d93290e
ash-silver <passlion@naver.com>
2023-01-17 12:45:06 +0900

    0117 before lunch temp

---
+++ Project/src/main/java/com/project/controller/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.service.PurchaseService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/purchase")
+public class PurchaseController {
+	
+	@Autowired
+	private PurchaseService purchaseService;
+	
+	@GetMapping("")
+	public String purchase() {
+		return "purchase";
+	}
+	
+	@PostMapping("")
+	public String addOrder(Order order)
+	
+}
---
+++ Project/src/main/java/com/project/model/OrderModel.java
@@ -0,0 +1,19 @@
+package com.project.model;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderModel {
+	private int o_id;
+	private String o_product_p_fk;
+	private String o_member_m_fk;
+	private String o_date;
+	private String o_quantity;
+	private String o_state;
+}
