2052b17cf0f72623f867c15e3a50dcb5f7b1fdc6
JaeYun Kim <lava124@naver.com>
2023-01-19 11:59:37 +0900

    검색 기본 기능 완료

--- src/main/resources/static/js/prodlist.js
+++ src/main/resources/static/js/prodlist.js
@@ -27,7 +27,7 @@ function drawList(list, num) {
 
 		html += `
 			<div class="list_box">
-				<a href="/prodetail?p_id=${posting.p_id}" class="list_box_url"> <img src="/stsimg/${row.p_img}">
+				<a href="/prodetail?p_id=${posting.p_id}" class="list_box_url"> <img src="/stsimg/${posting.p_img}">
 				</a>
 				<label class="list_box_name">
 					<a href="/prodetail?p_id=${posting.p_id}" >제품명:${posting.p_name}</a>
