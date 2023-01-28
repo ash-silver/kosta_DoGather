cc03b5c02ca29766654e6057334cd2998c4b0572
pows1011 <54159945+sopand@users.noreply.github.com>
2023-01-28 15:56:28 +0900

    Update ProductController.java
    재윤..코드의 상태가 이상하다..???

--- src/main/java/com/project/controller/ProductController.java
+++ src/main/java/com/project/controller/ProductController.java
@@ -18,12 +18,12 @@ import com.project.service.ProductService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor   //requestmapping으로 기본 매핑값 주는게..?
 public class ProductController {
 	
 	private final ProductService pService;
 	
-	
+	 // 이거는 왜 놔둔거임..? 우리 img db가있는데 imgsplit를 왜 써....
 	public PagingResponse<Product> imgsplit(PagingResponse<Product> list){		
 		if (!list.getList().isEmpty()) { // 나누기위해 저장시켜놓은 이미지 파일의 명을 잘라내기 위한 메서드. 페이징처리된 List에서 getImg값을 가져옴
 			for (int i = 0; i < list.getList().size(); i++) {
@@ -37,11 +37,11 @@ public class ProductController {
 	
 	/*   -------------------------메인페이지-----------------------------	 */
 	
-	@GetMapping("/")
+	@GetMapping("/") // 이거랑 하단에 GetMapping("/index")는 같은건데 왜 2개를 따로나눳냐 ... 매핑2개같이하는거 방법 찾아서 바꿔라..
 	public String mainPage(Model model) {
 		ArrayList<Product> bestlist = pService.Productbest();
 		ArrayList<Product> newlist = pService.Productnew();
-		for(int i=0;i<bestlist.size();i++) {
+		for(int i=0;i<bestlist.size();i++) { // 아니....xml에 조인도안했는데 왜 이미지를 Get해서 split하고있냐구요...
 			String[] bestimg = bestlist.get(i).getImg().get(i).getImg_name().split("/");//인기순
 			String[] newimg = newlist.get(i).getImg().get(i).getImg_name().split("/");//신상품순
 			bestlist.get(i).getImg().get(i).setImg_name(bestimg[0]);
@@ -72,19 +72,20 @@ public class ProductController {
 	
 	
 	/*   -------------------------페이징-----------------------------	 */
-	
+							// 저기 페이징에 왜 전부다 Mapping이름만 다르고 같은 Service를 가져오고있음...???
+							
 	@GetMapping("/newlist")
 	public String newlist(@ModelAttribute("params") SearchDto params, Model model, String p_category) {
 		PagingResponse<Product> plist = pService.PagingList(params);
 		
-		model.addAttribute("plist", imgsplit(plist));
+		model.addAttribute("plist", imgsplit(plist)); // split 쓰지마임마... 왜써 img db를 조인해서 가져올생각을 해야지
 		model.addAttribute("category", p_category);
 		
 		return "prodlist";
 	}
 	@GetMapping("/pricelist")
 	public String pricelist(@ModelAttribute("params") SearchDto params, Model model, String p_category) {
-		PagingResponse<Product> plist = pService.PagingList(params);
+		PagingResponse<Product> plist = pService.PagingList(params);  //
 		
 		model.addAttribute("plist", imgsplit(plist));
 		model.addAttribute("category", p_category);
